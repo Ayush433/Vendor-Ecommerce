b6b1a927b5f5dad021b266c4676846cd605d50a2
Ayush Adhikari <adhikariayush19@gmail.comgit config --global user.nameAyush433>
2023-07-03 13:06:17 +0545

    formik validation

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,97 @@
+import { useFormik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "Password must be at least 8 characters")
+        .required("Required"),
+    }),
+    onSubmit: async (values, action) => {
+      console.log(values);
+      action.resetForm();
+    },
+  });
+
   return (
     <>
-      <div className="min-h-screen bg-gray-50 flex flex-col py-12 justify-center sm:px-6 lg:px-8 ">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Login In Your Account
-          </h2>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="w-full max-w-md">
+          <form
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                name="email"
+                autoComplete="email"
+                placeholder="Enter your email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+              {formik.errors.email && formik.touched.email ? (
+                <h1 className="text-pink-700">{formik.errors.email}</h1>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+              {formik.errors.password ? (
+                <h1 className="text-red-700">{formik.errors.password} </h1>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Sign In
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="/forgot-password"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy; {new Date().getFullYear()} My App. All rights reserved.
+          </p>
         </div>
       </div>
     </>
