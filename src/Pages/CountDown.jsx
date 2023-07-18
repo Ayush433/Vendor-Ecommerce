e0476c359364646cff9f423e8650344659bfb845
Ayush Adhikari <adhikariayush19@gmail.com>
2023-07-18 10:01:29 +0545

    CountDown and popular event task running

---
+++ src/Pages/CountDown.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+
+const CountDown = () => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date("2023-08-01") - +new Date();
+    let timeLeft = {};
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor(((difference / 1000) * 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const timerComponents = Object.keys(timeLeft).map((interval) => {
+    if (!timeLeft[interval]) {
+      return null;
+    }
+    return (
+      <span className="text-[25px] text-[#475ad2] p-1" key={interval}>
+        {timeLeft[interval]}
+        {interval}
+        {""}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span className="text-[red] text-[25px] p-6"> Times Up</span>
+      )}
+    </div>
+  );
+};
+
+export default CountDown;
--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import CountDown from "./CountDown";
 
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className="">
+      <h1 className="font-bold p-6  text-3xl">Popular Events</h1>
+      <div className=" w-full md:w-[80%] md:ml-[150px] block bg-white rounded-lg lg:flex p-2 pb-7 ">
+        <div className="w-full lg:-w[50%] m-auto">
+          <img
+            src="https://m.media-amazon.com/images/I/31Vle5fVdaL.jpg"
+            alt="image"
+          />
+        </div>
+        <div className="w-full lg:-w[50%] flex flex-col justify-center md:mb-[250px] md:mt-[20px]">
+          <h2 className="text-3xl font-bold">Iphone 14 Pro Max</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nam
+            nihil eveniet esse, dolore numquam consectetur ducimus officiis
+            temporibus blanditiis molestiae itaque nemo velit exercitationem
+            assumenda voluptate? Illo, sint? Quas!
+          </p>
+          <div className="flex py-2 justify-between">
+            <div className="flex ">
+              <h5 className="font-[500] text-[18px] text-red-300 pr-3 line-through">
+                RS.1099
+              </h5>
+              <h5 className="font-bold text-[20px] font-Roboto text-[#333]">
+                Rs.999
+              </h5>
+            </div>
+            <span className="pr-3 font-semibold text-[17px] text-[#44a55e]">
+              120 Sold
+            </span>
+          </div>
+          <CountDown />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
